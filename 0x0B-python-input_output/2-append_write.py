fed9d112708f4c4fe7beb90e27f2cab19c73aed9
Assefa Endale <assefaendale7@gmail.com>
2023-10-14 15:46:31 -0700

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 '''
-Module contains a function for writing to a text file
+Module contains a function for appending to a text file
 '''
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     '''
-    Writes some text to a file
+    Appends some text to a file
     Args:
         filename (string): The name of the file to write to
         text (string): The text to add to the file
     '''
-    with open(filename, mode='w', encoding='utf-8') as f:
+    with open(filename, mode='a', encoding='utf-8') as f:
         return f.write(text)
